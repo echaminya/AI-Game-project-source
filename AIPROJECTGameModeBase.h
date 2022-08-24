5d1b4f8848403fb42f1c414b2b1ead2b14f3edf3
echaminya <jmbarani@gmail.com>
2022-08-24 22:34:25 +0300

    Add files via upload

---
+++ AIPROJECTGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AIPROJECTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPROJECT_API AAIPROJECTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ AITUTORIALGameModeBase.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "AIPROJECTGameModeBase.h"
+
